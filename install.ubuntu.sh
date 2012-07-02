3e11320d766f84389ecf6cf9333e98ce67f5b73e
Bhaskar Sundarraj <bhaskar.sundarraj@gmail.com>
2012-07-02 11:20:59 -0700

    Temp hack for installing java.  - need to be moved back as package.

--- install.ubuntu.sh
+++ install.ubuntu.sh
@@ -3,6 +3,9 @@
 #apt-get install rubygems
 gem install puppet --no-rdoc --no-ri
 
-
+add-apt-repository ppa:webupd8team/java
+apt-get install oracle-java7-installer"
 
 puppet apply -v --modulepath puppet/modules config.pp --debug
+source /etc/profile
+gem uninstall puppet

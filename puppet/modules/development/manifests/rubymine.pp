98ce54e6057316802072582d4f8591c77de1a77e
Bhaskar Sundarraj <bhaskar.sundarraj@gmail.com>
2012-07-02 09:25:27 -0700

    Adding Rubymine and dropbax deb packages.

--- puppet/modules/development/manifests/rubymine.pp
+++ puppet/modules/development/manifests/rubymine.pp
@@ -2,11 +2,11 @@ class development::rubymine {
 
  #    $packagedir = "../../packages/ubuntu"
 
- #    package { 'Rubymine':
- #      ensure => installed,
- #      source => "${packagedir}/rubymine_1.0_i386.deb",
- #      provider => 'dpkg',
- #    }
+     package { 'Rubymine':
+       ensure => installed,
+       source => "puppet:///modules/development/rubymine_1.0_i386.deb",
+     #  provider => 'dpkg',
+     }
 
 # http://download.jetbrains.com/ruby/RubyMine-4.0.3.tar.gz
 # https://rvm.beginrescueend.com/install/rvm.
